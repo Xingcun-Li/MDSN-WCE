4da28bbba0e15913df7b7dbc0bd7b5b5357b1135
Xingcun Li <81893440+Xingcun-Li@users.noreply.github.com>
2025-04-09 13:00:50 +0800

    Update plot_tSNE_CADCAP.py

--- capsule_model/plot_tSNE_CADCAP.py
+++ capsule_model/plot_tSNE_CADCAP.py
@@ -61,8 +61,8 @@ def get_args():
                         help="Random seed, default=42")
     parser.add_argument("--epochs",
                         type=int,
-                        default=30,
-                        help="Max training epochs, default=50")
+                        default=40,
+                        help="Max training epochs, default=40")
     parser.add_argument("--optimizer",
                         type=str,
                         default="SGD",
